43474ff237ea9f8dbb3618a6447501d817c3dd21
Eric Nelson <eric.mckenzie.nelson@gmail.com>
2011-04-11 18:46:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onegreensock01::Application.config.secret_token = '18dae87580cf6ff3e821052bf52252488baae365606ca84d9936f79cfa04c90018c6e36223be86154104ccebf9f158fcf7ea7cb9f83f7319f511033609ffa897'
